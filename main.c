e1ef0f58edd86b9be5eced185d6640809b6556d0
Pasi Oksa <pasi.oksa@gmail.com>
2012-04-12 20:23:41 +0300

    Modified pwm routine. Fix timer to trigger compare match interrupt for every 5us

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <avr/interrupt.h>
 #include <inttypes.h>
 
+#define F_CPU 9600000UL  // 9.6 MHz
+#include <util/delay.h>
+
 #define PIN_UP   (PORTB |=  (1<<3));
 #define PIN_DOWN (PORTB &= ~(1<<3));
 
@@ -12,36 +15,39 @@
 volatile uint16_t time;
 volatile uint16_t last;
 volatile uint16_t up_time;
-
+volatile uint8_t state;
 ISR(INT0_vect)
 {
-		if(MCUCR&0x3) {
+		if(state) {
 			up_time = time;
 			FALLING_EDGE_INTERRUPT;
+			state=0;
 		}else
 		{
+			//PORTB=~PORTB;
 			if(time > up_time) {
-				last=time - up_time-200;
+				last= time - up_time - 200-36; 
 			}else{
-				last = up_time - time-220;
+				last = up_time - time - 200 -36;
 			}
 			RISING_EDGE_INTERRUPT;
+			state=1;
 		}
 				
 }
 
 ISR(TIM0_COMPA_vect)
 {
+	//PORTB=~PORTB;
 	//Timer interrupt will be generated every 5us
 	time++;
-	
 }
 
 void init_timer()
 {
-	TCCR0A = 0;
-	TCCR0B = (1<<CS01);
-	OCR0A = 60;
+	TCCR0A = (1<<WGM01);
+	TCCR0B = (1<<CS00);
+	OCR0A = 48;
 	TIMSK0=(2<<1); //Enable compare match 0 interrupt
 	TCNT0=0;
 	
@@ -51,26 +57,37 @@ void init_timer()
 void init_ext_int()
 {
 		RISING_EDGE_INTERRUPT;
+		state=1;
 		GIMSK = (1<<6);
+}
+
+void delay(uint8_t t)
+{
+	for(uint8_t i=1; i<t;i++)
+	{ 
+		_delay_us(1);
+	}
 }
 
 int main()
 {
 	init_timer();
 	init_ext_int();
-	PORTB=(1<<3);
+	DDRB=(1<<3);
 	time=last=up_time=0;
 	sei();
+
 	while(1) { 
-		if(last>=36) {
-			if(last<=(36+128)) {
-				PIN_UP;
-			} else { 
-				if( time%(~((last-36)<<2)) == 0) { 
-					PIN_UP; 
-				}else{ 
-					PIN_DOWN; 
+		if(last > 0) {
+			if(last > 128 ) {
+				if (last < 300) {
+					PIN_UP;
 				}
+			} else { 
+				PIN_UP;
+				delay(last);
+				PIN_DOWN;
+				delay(128-last);	 
 			} 
 		}else { 
 				PIN_DOWN; 
